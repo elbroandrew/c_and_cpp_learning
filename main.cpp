8f255f6be85f05c1d170d4ada693f7300e53346e
Andrew <elbroandrew@gmail.com>
2023-03-13 14:19:44 +1000

    safe strcopy func

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     char name[40];
-    strcpy_s(name, sizeof(name), "Andrew");
+    strcpy_s(name, sizeof(name), "Andrew"); //use this safe strcopy func because of safety (avoid any garbage)
     cout << sizeof(name) << endl; //40 = 39 + \0
     cout << strlen(name) << endl; //6
     return 0;
