dc1873eb77952f28172a3db9e5ac24907c026bed
Andrew <elbroandrew@gmail.com>
2023-03-13 14:18:47 +1000

    sizeof and strlen

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
-    char name[40] = "andrew";
-    char *p = name;
-    int len = 0;
-    while (*p){
-        ++len; //increment length variable
-        ++p; //move pointer to next position
-    }
-    cout << len << endl;
-
+    char name[40];
+    strcpy_s(name, sizeof(name), "Andrew");
+    cout << sizeof(name) << endl; //40 = 39 + \0
+    cout << strlen(name) << endl; //6
     return 0;
 }
