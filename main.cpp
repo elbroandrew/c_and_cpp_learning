6ac037465504132231de05042727590880586a61
Andrew <elbroandrew@gmail.com>
2023-05-20 18:57:29 +1000

    массивы: коррект некоррект объявления

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 
 using namespace std;
-
+#define SIZE_BUFFER     1024
 int main() {
 
-    int g = 476789;
-    char *ptr = (char *)&g;
+    //корректные объявления
+    double f[30];
+    char buffer[SIZE_BUFFER];
+    int marks[12];
+    short ar[8 * 5];
+    char bytes[sizeof(double)];
 
-    for(int i = 0; i < sizeof(int); ++i){
-        printf("%d\n", *++ptr); //++ptr - прибавил и присвоил, а ptr++ прибавил, а значение не присвоил.
-    }
-    //70
-    //7
-    //0
-    //3
+    //некорректные объявления
+    int n = 5;
+    int array[n]; //C++99 можно, в других - по-разному.
+    int null_array[0]; //нежелательно
+
 
     return 0;
 }
