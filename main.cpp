f741558ed58c3087b80cc0a41fabe6b5c332cdc4
Andrew <elbroandrew@gmail.com>
2023-05-20 20:40:04 +1000

    массивы: инициализация массива

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
-#define SIZE_BUFFER     10
+#define SIZE_BUFFER     5
 int main() {
 
-    int buffer[SIZE_BUFFER];
+    int buffer[SIZE_BUFFER] = {1,2,3}; //а остальные сами станут 0 т.е. {1,2,3,0,0} - это инициализация, знак равно тут инициализирует, не присваивает.
+    
     size_t b = sizeof(buffer);
     size_t len = sizeof(buffer) / sizeof(int);
     printf("%zu", len);
-
-    //second variant
-    size_t len2 = sizeof(buffer) / sizeof(buffer[0]); //тоже применимо, т.к. ссылается на первый элемент массива
-    //third variant
-    size_t len3 = sizeof(buffer) / sizeof(*buffer);
 
     return 0;
 }
