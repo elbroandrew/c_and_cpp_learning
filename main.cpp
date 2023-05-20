a2eb43c7e1cee96282f9b54aa516c97610b42506
Andrew <elbroandrew@gmail.com>
2023-05-20 20:33:52 +1000

    массивы: size_t

--- main.cpp
+++ main.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
 
-using namespace std;
-#define SIZE_BUFFER     1024
+#define SIZE_BUFFER     10
 int main() {
 
-    //корректные объявления
-    double f[30];
     char buffer[SIZE_BUFFER];
-    int marks[12];
-    short ar[8 * 5];
-    char bytes[sizeof(double)];
-
-    //некорректные объявления
-    int n = 5;
-    int array[n]; //C++99 можно, в других - по-разному.
-    int null_array[0]; //нежелательно
-
+    size_t b = sizeof(buffer);
+    printf("%zu", b); //1 byte x 10 = 10; zu - is for 'size_t' type.
 
     return 0;
 }
