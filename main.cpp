b4f347afc49d1ec82c5356e58b883350ce2a8996
Andrew <elbroandrew@gmail.com>
2023-05-20 20:42:17 +1000

    массивы: инициализация массива 0

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #define SIZE_BUFFER     5
 int main() {
 
-    int buffer[SIZE_BUFFER] = {1,2,3}; //а остальные сами станут 0 т.е. {1,2,3,0,0} - это инициализация, знак равно тут инициализирует, не присваивает.
-    
+    int buffer[SIZE_BUFFER]; // можно иниц нулём, лучше вот так ={0}, чем просто ={} .
+    buffer = {1,2,3,4,5}; // error, это уже присваивание, так нельзя. Только могу присвоить значение buffer[5] = 244;
     size_t b = sizeof(buffer);
     size_t len = sizeof(buffer) / sizeof(int);
     printf("%zu", len);
