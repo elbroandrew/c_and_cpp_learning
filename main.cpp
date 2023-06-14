83b9b04a049d6c68e355c62a7d8081bf792ad510
andrew <elbroandrew@gmail.com>
2023-06-14 22:00:16 +1100

    use threads

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
+#include <thread>
 
-using namespace std;
-#define SIZE_BUFFER     1024
+void hello(){
+  std::cout << "Hello concurrnet World!\n";
+}
+
+
 int main() {
-
-    //корректные объявления
-    double f[30];
-    char buffer[SIZE_BUFFER];
-    int marks[12];
-    short ar[8 * 5];
-    char bytes[sizeof(double)];
-
-    //некорректные объявления
-    int n = 5;
-    int array[n]; //C++99 можно, в других - по-разному.
-    int null_array[0]; //нежелательно
-
-
-    return 0;
+  std::thread t(hello);
+  t.join();
+  return 0;
 }
